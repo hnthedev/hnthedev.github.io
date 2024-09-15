925fa18699e6f944e5a17dcd40050b753a6ab8f8
Hoang Nguyen <88585967+hnthedev@users.noreply.github.com>
2024-09-15 20:37:11 +0700

    huge changes

---
+++ script.js
@@ -0,0 +1,23 @@
+window.setTimeout(function() {
+    document.getElementById('box').style.opacity = '1';
+}, 100);
+
+const AboutBtn = document.getElementById("AboutBtn");
+var CurrentlyOpen = false;
+
+AboutBtn.addEventListener('click', function() {
+    if (!window.matchMedia('(max-width: 720px)').matches && !CurrentlyOpen) {
+        document.getElementById('box').style.transform='translate(50%,-50%)';
+        CurrentlyOpen = true;
+    }; 
+    document.getElementById('information').style.opacity = 1;
+})
+
+window.addEventListener("resize", function(event) {
+    if (window.matchMedia('(max-width: 720px)').matches && CurrentlyOpen) {
+        document.getElementById("box").style.transform="initial";
+    }
+    if (window.matchMedia('(min-width: 730px)').matches && CurrentlyOpen) {
+        document.getElementById('box').style.transform='translate(50%,-50%)';
+    }
+})
