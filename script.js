593112bfc61b5b85c7820a17180df5b8260ef4b4
Hoang Nguyen <88585967+hnthedev@users.noreply.github.com>
2024-09-16 18:47:54 +0700

    huge

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.setTimeout(function() {
 
 var
 aboutHTML = `
-<h1 style="font-weight: 900;">About Me</h1><br>
+<h1>About Me</h1><br>
 <p style="color: #f2f2f299;line-height:1.5;">
 My name's Hoang Nguyen and I'm from Vietnam. I'm currently a high school student who is trying
 to learn something new.
@@ -14,9 +14,32 @@ I learned a lot of programming languages. My main language is Python :D
 </p>`,
 
 
-
 ProjectsHTML = `
-<h1>My Projects</h1>`,
+<h1>My Projects</h1><br>
+<p style="color: #f2f2f299;line-height:1.5;">
+I created a lot of projects since i was 14 years old. Most of them are just normal
+games like Flappy Bird, 2D ver of Subway Sufers, Clock GUI and some platformer games.
+<br><br>
+But when i got a bit older, these are the projects that im really proud of:<br>
+<div style="width=100%; padding-bottom: 10px;">
+    <img src="images/hnstore.jpg" alt="hn's store checker" width="25%" style="float: left;padding: 5px;padding-right: 20px;max-height: 70px;max-width: 70px;">
+    <h3 style="font-weight: 900;margin: 0px 10px;padding-top: 10px;padding-left: 20px;">hn's store checker</h3>
+    <p style="text-align: left;color: #f2f2f299;font-size: 0.72rem;margin-top: 5px;">a software that get user's daily store<br> in a game called Valorant</p>
+</div>
+<div style="width=100%; padding-bottom: 10px;">
+    <img src="images/chemistry.png" alt="hn's store checker" width="25%" style="float: left;padding: 5px;padding-right: 20px;max-height: 70px;max-width: 70px;">
+    <h3 style="font-weight: 900;margin: 0px 10px;padding-top: 10px;padding-left: 20px;">Hoá Học (Chemistry)</h3>
+    <p style="text-align: left;color: #f2f2f299;font-size: 0.72rem;margin-top: 5px;">a software that help secondary students<br> learn mostly anything about chemistry</p>
+</div>
+<div style="width=100%; padding-bottom: 10px;">
+    <img src="images/hnoverlay.png" alt="hn's store checker" width="25%" style="float: left;padding: 5px;padding-right: 20px;max-height: 70px;max-width: 70px;">
+    <h3 style="font-weight: 900;margin: 0px 10px;padding-top: 10px;padding-left: 20px;">hn's overlay</h3>
+    <p style="text-align: left;color: #f2f2f299;font-size: 0.72rem;margin-top: 5px;">a software that get other player's<br>stats when joining a valorant match</p>
+</div>
+
+
+
+</p>`,
 ContactHTML = `Contact`,
 FunHTML = `Fun`
 
@@ -56,7 +79,7 @@ AboutBtn.addEventListener('click', function() {
         document.getElementById("information").innerHTML = aboutHTML;
         document.getElementById("information").style.opacity = 1;
       }, 300);
-    if (!window.matchMedia('(max-width: 720px)').matches && !CurrentlyOpen) {
+    if (!window.matchMedia('(max-width: 1200px)').matches && !CurrentlyOpen) {
         document.getElementById('box').style.transform='translate(50%,-50%)';
         CurrentlyOpen = true;
     } else {
@@ -71,7 +94,7 @@ ProjectsBtn.addEventListener('click', function() {
         document.getElementById("information").style.opacity = 1;
       }, 300);
       
-    if (!window.matchMedia('(max-width: 720px)').matches && !CurrentlyOpen) {
+    if (!window.matchMedia('(max-width: 1200px)').matches && !CurrentlyOpen) {
         document.getElementById('box').style.transform='translate(50%,-50%)';
         CurrentlyOpen = true;
     } else {
@@ -85,7 +108,7 @@ ContactBtn.addEventListener('click', function() {
         document.getElementById("information").innerHTML = ContactHTML;
         document.getElementById("information").style.opacity = 1;
       }, 300);
-    if (!window.matchMedia('(max-width: 720px)').matches && !CurrentlyOpen) {
+    if (!window.matchMedia('(max-width: 1200px)').matches && !CurrentlyOpen) {
         document.getElementById('box').style.transform='translate(50%,-50%)';
         CurrentlyOpen = true;
     } else {
@@ -99,7 +122,7 @@ FunBtn.addEventListener('click', function() {
         document.getElementById("information").innerHTML = FunHTML;
         document.getElementById("information").style.opacity = 1;
       }, 300);
-    if (!window.matchMedia('(max-width: 720px)').matches && !CurrentlyOpen) {
+    if (!window.matchMedia('(max-width: 1200px)').matches && !CurrentlyOpen) {
         document.getElementById('box').style.transform='translate(50%,-50%)';
         CurrentlyOpen = true;
     } else {
@@ -108,10 +131,10 @@ FunBtn.addEventListener('click', function() {
 })
 
 window.addEventListener("resize", function(event) {
-    if (window.matchMedia('(max-width: 720px)').matches && CurrentlyOpen) {
+    if (window.matchMedia('(max-width: 1200px)').matches && CurrentlyOpen) {
         document.getElementById("box").style.transform="initial";
     }
-    if (window.matchMedia('(min-width: 730px)').matches && CurrentlyOpen) {
+    if (window.matchMedia('(min-width: 1230px)').matches && CurrentlyOpen) {
         document.getElementById('box').style.transform='translate(50%,-50%)';
     }
 })
